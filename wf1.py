f2d8aac513761b119edfdf436ac9acb9c68c59b7
Bhaskar Krishnamachari <bkrishna@usc.edu>
2023-02-08 23:48:59 -0800

    Update wf1.py

--- wf1.py
+++ wf1.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def waterfilling_solution(ni_gi, P_total):
+    '''this function returns the optimal 1/lambda*'''
     ni_gi = np.array(ni_gi)
-
-    for i in range(100):
-      inv_lambda_star = i*0.1
+    granularity = 10000
+    #this loop keeps increasing 1/lambda till powers add up to P_total
+    for i in range(granularity):
+      inv_lambda_star = i/granularity
       Parray = inv_lambda_star*np.ones(len(ni_gi))-ni_gi 
       Parray[Parray<0] = 0
       Ps = np.sum(Parray)
@@ -16,12 +18,16 @@ def waterfilling_solution(ni_gi, P_total):
     return inv_lambda_star
 
 def plot_waterfilling(ni_gi, P_total):
+    '''this function plots the waterfilling solution'''
     inv_lambda_star = waterfilling_solution(ni_gi, P_total)
     fig, ax = plt.subplots()
     x = np.arange(len(ni_gi))
     bars = ax.bar(x, ni_gi, color='blue')
-    powers = inv_lambda_star*np.ones(len(ni_gi))-ni_gi
+
+    #the following calculates the optimal power levels
+    powers = inv_lambda_star*np.ones(len(ni_gi))-ni_gi 
     powers[powers<0] = 0
+
     bars2 = ax.bar(x, powers, color='yellow', bottom=ni_gi)
     line = ax.axhline(inv_lambda_star, color='red', linestyle='--')
     for i, bar in enumerate(bars):
